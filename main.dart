c6eb5299d802e0a5be9a4b1e79817fe976484c74
swe444proj <abdr8147@gmail.com>
2024-11-05 13:54:02 +0300

    Update main.dart sprint3

--- main.dart
+++ main.dart
@@ -1,4 +1,4 @@
-mport 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:tic_tactics/auth.dart';
 import 'package:firebase_core/firebase_core.dart';
